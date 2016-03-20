878b1f0228226c049182a7cbc2c962d5d789888f
Luis Crespo <lcrespom@gmail.com>
2016-03-20 19:12:12 +0100

    Detect WriteNonLocal side effect

--- src/purecheck.js
+++ src/purecheck.js
@@ -23,36 +23,82 @@ function filterTree(statements, check) {
 }
 
 function checkFunc(fdec) {
-	var localVars = getLocalVars(fdec.body.body);
+	var locals = getLocalVars(fdec.body.body);
 	return {
 		loc: fdec.loc,
 		id: fdec.id ? fdec.id.name : null,
-		locals: localVars,
-		sideCauses: [],
-		sideEffects: []
+		locals,
+		sideCauses: checkSideCauses(fdec.body.body, locals),
+		sideEffects: checkSideEffects(fdec.body.body, locals)
 	};
 }
 
 function getLocalVars(statements) {
 	var decs = statements.filter(stmt => stmt.type == 'VariableDeclaration');
-	var locals = [];
+	var locals = {};
 	decs.forEach(dec =>
 		dec.declarations.filter(d => d.type == 'VariableDeclarator')
-			.forEach(vd => locals.push(vd.id.name))
+			.forEach(vd => locals[vd.id.name] = true)
 	);
 	return locals;
 }
 
+function checkSideCauses(statements, locals) {
+	var result = [];
+	statements.forEach(stmt => {
+		//TODO recursively scan all expressions (phew!)
+		//TODO check function calls with side causes
+		//TODO check function calls from blacklist
+	});
+	return result;
+}
+
+function checkSideEffects(statements, locals) {
+	var result = [];
+	statements.forEach(stmt => {
+		if (isAssignment(stmt)) {
+			var ident = getAssignmentTarget(stmt);
+			if (ident && !locals[ident]) {
+				result.push({
+					type: 'WriteNonLocal',
+					ident,
+					loc: stmt.loc
+				});
+			}
+		}
+		//TODO check function calls with side effects
+		//TODO check function calls from blacklist
+	});
+	return result;
+}
+
+function isAssignment(stmt) {
+	return stmt.type == 'ExpressionStatement' &&
+		stmt.expression.type == 'AssignmentExpression';
+}
+
+function getAssignmentTarget(stmt) {
+	var ident = null;
+	if (stmt.expression.left.type == 'Identifier')
+		ident = stmt.expression.left.name;
+	else if (stmt.expression.left.type == 'MemberExpression') {
+		if (stmt.expression.left.object.type == 'Identifier')
+			ident = stmt.expression.left.object.name;
+		else if (stmt.expression.left.object.type == 'ThisExpression')
+			ident = 'this';
+	}
+	return ident;
+}
 
 /*
 	Side causes:
 		- Reading a non-local variable
 		- Reading from this (optional, default to false)
-		- Invoking a function with side causes
+		- Invoking a function with side causes (according to previous scan)
 		- Invoking a function from a blacklist
 	Side effects:
 		- Writing to a non-local variable
 		- Writing to a parameter member (including this)
-		- Invoking a function with side effects
+		- Invoking a function with side effects (according to previous scan)
 		- Invoking a function from a blacklist
 */
