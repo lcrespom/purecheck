3951d7dde49cc29f9797e91ffb588b7b21ebcea1
Luis Crespo <lcrespom@gmail.com>
2016-12-27 20:14:11 +0100

    Improved side cause detection

--- src/purecheck.ts
+++ src/purecheck.ts
@@ -92,7 +92,7 @@ function walkTree(tree: Program, errors: FPError[]) {
 			case 'UpdateExpression':
 				return checkAssignOrUpdate(node, errors);
 			case 'Identifier':
-				//return checkSideCause(node, errors);
+				return checkSideCause(node, errors);
 		}
 	});
 }
@@ -129,14 +129,24 @@ function skipSideCause(node): boolean {
 	if (!node.parent) return true;
 	// Skip function declaration identifiers
 	if (node.parent.type == 'FunctionDeclaration') return true;
-	// Skip member expressions e.g. "obj.ident" (except leftmost part)
-	if (node.parent.type == 'MemberExpression'
-		&& node.parent.property == node) return true;
+	// Skip function invocations (to be checked elsewhere)
+	if (node.parent.type == 'CallExpression') return true;
+	// Skip if update expression (handled by side effect)
+	if (node.parent.type == 'UpdateExpression') return true;
+
 	// Skip if left side of direct assignment
 	if (node.parent.type == 'AssignmentExpression'
 		&& node.parent.left == node) return true;
+
+	// Skip object property identifiers e.g. "obj.prop",
+	// But catch computed properties, e.g. "obj[prop]"
+	if (node.parent.type == 'MemberExpression'
+		&& node.parent.property == node) return !node.parent.computed;
+
 	if (!node.parent.parent) return false;
+	// TODO should climb tree until type != 'MemberExpression'
 	// If we are here, only consider skipping composite assignment expressions
+	// TODO also consider type == 'UpdateExpression'
 	if (node.parent.parent.type != 'AssignmentExpression') return false;
 	// Skip if left side of composite assignment (e.g. x.y = ...)
 	return node.parent.parent.left == node.parent;
